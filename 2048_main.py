4babbe3f413b7f52f3002e524620a7cb5301ed0e
cmargel <81384335+cmargel@users.noreply.github.com>
2021-04-26 18:38:26 -0400

    Update 2048_main.py

--- 2048_main.py
+++ 2048_main.py
@@ -1,121 +1,239 @@
+# sources:
+#https://www.youtube.com/watch?v=HTJCUBp_S2I
+#https://www.youtube.com/watch?v=qwJ9w5bmKZU&t=1744s
+#https://www.youtube.com/watch?v=u2y06AOqKHc
+#https://pythonprogramming.net/pygame-python-3-part-1-intro/
+
+
 import random as r
+import pygame
+import sys
 
-def initialize_game():
-  grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-  rules = "Enter w to shift the board up \n Enter s to shift the board right \n Enter z to shift the board down \n Enter a to shift the board left" 
-  for i in range(2):
-    add_new_tile(grid)
-  print(rules)
-  print_grid(grid)
-  while keep_playing(grid):
-    make_move(grid)
-  
-def print_grid(grid):
-  #print each row of the grid on a new line- https://www.kite.com/python/answers/how-to-print-a-list-of-lists-in-columns-in-python
-  list_length = [len(str(element)) for row in grid for element in row]
-  col_width = max(list_length)
+def printGrid(grid):
+    print('-'*10)
+    for row in grid:
+        print(*row)
+    print('-'*10)
 
-  for row in grid:
-      row = "".join(str(element).ljust(col_width+2) for element in row)
-      print(row)
+def drawDisplay(score, change_score=0):
+    pygame.draw.rect(screen, WHITE, REC)
+    font = pygame.font.SysFont("stxingkai", 70)  
+    font_score = pygame.font.SysFont("simaun", 48)
+    text_score = font_score.render("Score: ", True, COLOR_TEXT)
+    text_score_value = font_score.render(f"{score}", True, COLOR_TEXT)
+    font_win = pygame.font.SysFont("simuan", 48)
+    text_win = font_win.render("You win!", True, COLOR_TEXT)
+    screen.blit(text_score, (20, 35))
+    screen.blit(text_score_value,(175,35))
+    if win(grid) == True:
+        screen.blit(text_win, (275, 35))
+    if change_score > 0:
+        text_change_score = font_score.render(f"+{change_score}", True, COLOR_TEXT)
+        screen.blit(text_change_score, (170, 65))
+    printGrid(grid)
 
-def add_new_tile(grid):
-  #choose random position on grid
-  x = r.randint(0,3)
-  y = r.randint(0,3)
-  tile_value = r.randint(0,1)
-  tile_added = False
-  while tile_added == False:
+    for row in range(BLOCKS):
+        for col in range(BLOCKS):
+            value = grid[row][col]
+            text = font.render(f'{value}',True,BLACK) 
+            w = col*SIZE_BLOCK + (col+1)*MARGIN
+            h = row*SIZE_BLOCK + (row+1)*MARGIN + SIZE_BLOCK
+            pygame.draw.rect(screen, COLORS[value], (w,h,SIZE_BLOCK,SIZE_BLOCK))
+            if value != 0:
+                font_w, font_h = text.get_size()
+                text_x = w + (SIZE_BLOCK-font_w)/2
+                text_y = h + (SIZE_BLOCK-font_h)/2
+                screen.blit(text, (text_x, text_y))
+
+def getNum(i, j):
+    return i*4+j+1
+
+def getIndex(num):
+    num -= 1
+    x,y = num//4, num%4
+    return x,y
+
+def addNewNum(grid,x,y):
+    if r.random() <= 0.75:
+        grid[x][y] = 2
+    else:
+        grid[x][y] = 4
+    return grid
+
+def getZeroes(grid):
+    zeroes = []
     for i in range(4):
         for j in range(4):
-            if grid[x][y] == 0:
-                if tile_value == 0:
-                    grid[x][y] = 2
-                    tile_added = True
-                else:
-                    grid[x][y] = 4
-                    tile_added = True
-            False
+            if grid[i][j]==0:
+                num = getNum(i,j)
+                zeroes.append(num)
+    return zeroes
 
-
-def make_move(current_grid):
-  move = input("Which direction do you want to shift the board? ")
-
-  if move == "w":
-    print("move up")
-  elif move == "s":
-    move_right(current_grid)
-  elif move == "z":
-    print("move down")
-  elif move == "a":
-    print("move left")
-  else:
-    print("Invalid key. Enter a valid move.")
-    make_move(current_grid)
-
-#shifts tiles to the right without combining them
-def push_board(grid):
-   new_grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-   done = False
-   for x in range(4):
-      count = 3
-      for y in range(count, -1, -1):
-         if grid[x][y] != 0:
-            new_grid[x][count] = grid[x][y]
-            if y != count:
-               done = True
-            count -= 1
-   return (new_grid, done)
-
-#add together and merge tiles with the same value
-def merge(grid):
-   score = 0
-   done = False
-   cells = 4
-   for x in range(cells):
-      for y in range(cells -1, 0 , -1):
-         if grid[x][y] == grid[x][y-1] and grid[x][y] != 0:
-            grid[x][y] = grid[x][y] * 2
-            score += grid[x][y]
-            grid[x][y-1]=0
-            done = True
-   return (grid, done)
-
-def move_right(current_grid):
-  new_grid,done = push_board(current_grid)
-  new_grid,done = merge(new_grid)
-  new_grid,done = push_board(new_grid)
-  add_new_tile(new_grid)
-  print_grid(new_grid)
-  return new_grid
-
-def keep_playing(grid):
-  for x in range(4):
-    for y in range(4):
-      #User has reached 2048 on a tile
-      if(grid[x][y] == 2048):
-        print("You win!")
-        return False
-      #There is at least one empty space on the grid left
-      elif(grid[x][y] == 0):
-        return True
-      else:
-        #Merging in one direction will still combine some tiles
-        for x in range(3):
-          for y in range(3):
-            if(grid[x][y]==grid[x+1][y] or grid[x][y]==grid[x][y+1]):
-              return True
-        for x in range(3):
-          if(grid[x][3] == grid[x+1][x]):
+def win(grid):
+    for row in grid:
+        if 2048 in row:
             return True
-        for y in range(3):
-          if(grid[3][y] == grid[3][y+1]):
+    return False
+
+def zero_in_grid(grid):
+    for row in grid:
+        if 0 in row:
             return True
-        print("No more moves. You lose.")
-        return False
+    return False
+
+def move_left(grid):
+    change_score = 0
+    for row in grid:
+        while 0 in row:
+            row.remove(0)
+        while len(row) != 4:
+            row.append(0)
+    for i in range(4):
+        for j in range(3):
+            if grid[i][j] == grid[i][j+1] and grid[i][j] != 0:
+                grid[i][j] *= 2
+                change_score += grid[i][j]
+                grid[i].pop(j+1)
+                grid[i].append(0)
+    return grid, change_score
+
+def move_right(grid):
+    change_score = 0
+    for row in grid:
+        while 0 in row:
+            row.remove(0)
+        while len(row) != 4:
+            row.insert(0,0)
+    for i in range(4):
+        for j in range(3, 0, -1):
+            if grid[i][j] == grid[i][j-1] and grid[i][j] != 0:
+                grid[i][j] *= 2
+                change_score += grid[i][j]
+                grid[i].pop(j-1)
+                grid[i].insert(0,0)
+    return grid, change_score
+
+def move_up(grid):
+    change_score = 0
+    for j in range(4):
+        col = []
+        for i in range(4):
+            if grid[i][j] != 0:
+                col.append(grid[i][j])
+        while len(col) != 4:
+            col.append(0)
+        for i in range(3):
+            if col[i] == col[i+1] and col[i] != 0:
+                col[i] *=2 
+                change_score += grid[i][j]
+                col.pop(i+1)
+                col.append(0)
+        for i in range(4):
+            grid[i][j] = col[i]
+    return grid, change_score
+
+def move_down(grid):
+    change_score = 0
+    for j in range(4):
+        col = []
+        for i in range(4):
+            if grid[i][j] != 0:
+                col.append(grid[i][j])
+        while len(col) != 4:
+            col.insert(0,0)
+        for i in range(3,0,-1):
+            if col[i] == col[i-1] and col[i] != 0:
+                col[i] *= 2
+                change_score += grid[i][j]
+                col.pop(i-1)
+                col.insert(0,0)
+        for i in range(4):
+            grid[i][j] = col[i]
+    return grid, change_score
 
 
-initialize_game()
+def can_move(grid):
+    for i in range(3):
+        for j in range(3):
+            if grid[i][j] == grid[i][j+1] or grid[i][j] == grid[i+1][j]:
+                return True
+    return False            
+
+
+grid = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+COLOR_TEXT = (255, 127, 0)
+COLORS = {
+    0: (204, 192, 179),
+    2: (238, 228, 219),
+    4: (240, 226, 202),
+    8: (242, 177, 121),
+    16: (236, 141, 85),
+    32: (250, 123, 92),
+    64: (234, 90, 56),
+    128: (237, 207, 114),
+    256: (242, 208, 75),
+    512: (237, 207, 114),
+    1024: (227, 186, 19),
+    2048: (236, 192, 2),
+    4096: (204, 204, 0),
+    8192: (153, 153, 0),
+    16384: (102, 102, 0)
+
+}
+
+
+WHITE = (255, 255, 255)
+GRAY = (130, 130, 130)
+BLACK = (0, 0, 0)
+BLOCKS = 4
+SIZE_BLOCK = 110
+MARGIN = 10
+WIDTH = BLOCKS*SIZE_BLOCK + (BLOCKS+1)*MARGIN
+HEIGHT = WIDTH+110
+REC = pygame.Rect(0,0,WIDTH,110)
+score = 0
+
+#grid[1][2] = 2
+#grid[3][0] = 4
+#print(getZeroes(grid))
+printGrid(grid)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
+drawDisplay(score)
+pygame.display.update()
+while zero_in_grid(grid) or can_move(grid):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            change_score = 0
+            if event.key == pygame.K_LEFT:
+                grid, change_score = move_left(grid)
+            elif event.key == pygame.K_RIGHT:
+                grid, change_score = move_right(grid)
+            elif event.key == pygame.K_UP:
+                grid, change_score = move_up(grid)
+            elif event.key == pygame.K_DOWN:
+                grid, change_score = move_down(grid)
+            score += change_score
+            drawDisplay(score)
+            zeroes = getZeroes(grid)
+            r.shuffle(zeroes)
+            r_num = zeroes.pop()
+            x, y = getIndex(r_num)
+            grid = addNewNum(grid, x, y)
+            print(r_num)
+            drawDisplay(score, change_score)
+            pygame.display.update()
 
 
 
